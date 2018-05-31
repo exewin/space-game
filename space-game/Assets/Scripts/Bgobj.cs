a79170b45928e9916551d3ee2e822c8e2dc58f0f
exewin <windowsmemphis@gmail.com>
2018-05-31 21:37:33 +0200

    [v101 3] gpx scripts
    zotron
    bgobjs: 1,2
    scripts: bgobj, bgobjspawner

---
+++ space-game/Assets/Scripts/Bgobj.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bgobj : MonoBehaviour 
+{
+
+	public bool randomColor;
+	public bool randomScale;
+	public bool randomRot;
+	
+	float scale = 1;
+
+	void Start() 
+	{
+		if(randomColor)
+			GetComponent<SpriteRenderer>().color = new Color(Random.Range(0,.7f),Random.Range(0,.7f),Random.Range(0,.7f),1);
+		
+		if(randomScale)
+		{
+			scale = Random.Range(0.5f,3f);
+			transform.localScale = new Vector3(scale,scale,scale);
+			GetComponent<SpriteRenderer>().sortingOrder = (int)(-200+(scale*16));
+		}
+		
+		if(randomRot)
+		{
+			Quaternion rotation = Quaternion.Euler(0, 0, Random.Range(0,360f));
+			transform.rotation = rotation;
+		}
+	}
+	
+	void Update()
+	{
+		transform.Translate(0,-scale*Time.deltaTime,0,Space.World);
+	}
+}
---
+++ space-game/Assets/Scripts/BgobjSpawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BgobjSpawner : MonoBehaviour 
+{
+
+	public GameObject[] bgobjs;
+	float timer;
+	float timeSpawn;
+
+	void Update () 
+	{
+		if(timer>timeSpawn)
+		{
+			Instantiate(bgobjs[Random.Range(0,bgobjs.Length)],new Vector3(Random.Range(-56,-136),0,0),transform.rotation);
+			timer=0;
+		}
+		else
+		{
+			timer+=Time.deltaTime*1;
+			timeSpawn = Random.Range(10,25);
+		}
+	}
+}
+
